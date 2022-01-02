0e5c78b95233df3dcf661336f40718763512933e
darky <growlbox123g@gmail.com>
2022-01-02 22:00:32 +0100

    unnecesary things.exe

--- pages/jizdnirady.tsx
+++ pages/jizdnirady.tsx
@@ -10,7 +10,7 @@ export const getStaticProps = async () => {
     const res = await fetch('https://www.idsjmk.cz/api/departures/busstop-by-name?busStopName=Klobouky%20u%20Brna%2C%20n%C3%A1m%C4%9Bst%C3%AD');
     const data = await res.json();
     var array = [];
-    var number;
+    
     /* const Style1 = {
         gridColumnStart: 1,
         gridColumnEnd: 2,
@@ -30,22 +30,16 @@ export const getStaticProps = async () => {
     for(var init = 0;init < array.length; init++){
         array[init];
     }
-    for (var inits = 1; inits <= array.length; inits++) {
-        number = inits;
-    }
     console.log(array.length);
     return {
         props: { bus: array, 
-/*                  Style1: Style1,
-            Style2: Style2,
-            Style3: Style3,
-*/            number: number,
+         
                 }
     }
 }
 
 
-const Idsjmk = ({ bus, Style1, Style2, Style3, number }) => {
+const Idsjmk = ({ bus }) => {
     return (
             <div className={Grid.grid}>
             <Uppernav />
